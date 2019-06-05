225164c4da99448a389986ba30c1abeac257ffde
richardsderek <richards.derek@gmail.com>
2019-06-05 15:32:29 +0200

    Create tabelle.inc.php

---
+++ TermineFullCal/includes/tabelle.inc.php
@@ -0,0 +1,34 @@
+<?php
+
+require "dbh.inc.php";
+
+// Tabelle erstellen (mySQLi Procedural)
+$sql = "CREATE TABLE IF NOT EXISTS prob
+(
+  probID int(255) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  nachname varchar(255) NOT NULL,
+  vorname varchar(255) NOT NULL,
+  geburtsdatum varchar(255) NOT NULL
+)";
+if (mysqli_query($conn, $sql)) {
+} else {
+  echo "Erstellung der Tabelle fehlgeschlagen: " .mysqli_error($conn) ."<br>";
+}
+
+$sql = "CREATE TABLE IF NOT EXISTS tblTermine
+(
+  id int(255) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  start varchar(255) NOT NULL,
+  end varchar(255) NOT NULL,
+  title varchar(255) NOT NULL
+  )";
+if (mysqli_query($conn, $sql)) {
+} else {
+  echo "Erstellung der Tabelle fehlgeschlagen: " .mysqli_error($conn) ."<br>";
+}
+
+
+
+
+
+ ?>
