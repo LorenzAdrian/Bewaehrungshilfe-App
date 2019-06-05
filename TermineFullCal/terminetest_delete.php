94c4ac5dde26964c860ee2e687a226bcd1a550ce
richardsderek <richards.derek@gmail.com>
2019-06-05 16:02:58 +0200

    Create terminetest_delete.php

---
+++ TermineFullCal/terminetest_delete.php
@@ -0,0 +1,27 @@
+<?php
+
+require "includes/tabelle.inc.php";
+
+
+// Eingetragene ID-Nummer von terminetestdelete.html
+$id = $_POST['terId'];
+
+// Werte löschen mit "prepared statements" (mySQLi Procedural)
+$sql = "DELETE FROM tblTermine WHERE id = ?";
+$stmt = mysqli_stmt_init($conn);
+if (mysqli_stmt_prepare($stmt, $sql)) {
+  mysqli_stmt_bind_param($stmt, "s", $id);
+  mysqli_stmt_execute($stmt);
+  echo "Termin erfolgreich geslöscht<p>";
+
+  // Button um Zurück zur terminform.html-Seite zu gehen
+  echo "<form action ='terminetestdelete.html' method ='post'>";
+  echo "<input type ='submit' value ='Weitere Termine löschen'>";
+  echo "</form> <p>";
+
+  echo "<a href=\"fullcalendar/fullcalendar.php\">Kalender</a>";
+} else {
+    echo "Fehler: " .$sql . "<br>" .mysqli_error($conn);
+}
+
+ ?>
