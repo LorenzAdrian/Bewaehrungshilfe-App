c687550496944162f836e227a50de3d373fdd59c
AminaDr <50828174+AminaDr@users.noreply.github.com>
2019-06-10 20:09:08 +0200

    Add files via upload
    Aktuelle Version Stand: 10.06.19

--- App-CSS/test.php
+++ App-CSS/test.php
@@ -1,51 +1,55 @@
 <!DOCTYPE html>
 <html lang="de" dir="ltr">
-    
+
 <head>
-    
+
     <meta charset="utf-8">
-    
-    <!--- Pfad zur style.css--------------------------->    
+
+    <!--- Pfad zur style.css--------------------------->
     <link rel="stylesheet" href="style.css">
-    <!------------Schriftart aus google fonts------------------>
+    <!--Schriftart aus google fonts------------------>
     <link href="https://fonts.googleapis.com/css?family=Raleway&display=swap" rel="stylesheet">
-    
+
     <title>Start</title>
-    
+
 </head>
- 
-<body>  
+
+<body>
 
 <div class="logo"></div>
-    
+
 <div class="hallobox">Hallo...</div>
-    
+
 <div class="menu">
-            
+
             <li><a href ="https://cssgridgarden.com/#de"><img src="img/search.svg">Suchen</a>
-            </li>  
-            
+            </li>
+
             <li><a href ="#"><img src="img/user-circle.svg">Mein Bereich</a>
             </li>
-            
+
 </div>
-    
+
 <div class="uberschrift1">Meine Probanden</div>
 
 <div class="uberschrift2">Mein Kalender</div>
-        
+
 <div class="rahmen">
     <?php include'includes/listeprob.inc.test.php';?>
 </div>
-    
+
 <div class="calendar">
     <?php include 'FullCalendar.php';?>
 </div>
-    
+
 <div class="button">
     <a href="#" class="button">Proband hinzufügen</a>
 </div>
 
+<div class="knopf">
+    <a href="#" class="knopf">Proband löschen</a>
+</div>
+
 </body>
-    
+
 </html>
