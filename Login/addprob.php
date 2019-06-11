301e74f53e36f0a3c4c2ab109520c3ce1450a9f7
Playalol <51054987+Playalol@users.noreply.github.com>
2019-06-11 08:40:24 +0200

    Update addprob.php

--- Login/addprob.php
+++ Login/addprob.php
@@ -1,1 +1,11 @@
 
+<main>
+
+    <h1>Signup</h1>
+    <form  action="includes/addprob.inc.php" method="POST">
+      <input type="text" name="vorname" placeholder="Vorname">
+      <input type="text" name="nachname" placeholder="Nachname">
+      <input type="text" name="geburtsdatum" placeholder="1985-04-25">
+      <button type="submit" name="addprob-submit">Signup</button>
+    </form>
+</main>
