de82ce5b19a8f6566dd1ef325d6a65d5f1729bad
Uecki <51044891+Uecki@users.noreply.github.com>
2019-06-16 21:01:35 +0200

    scriptLogin.js wieder hinzugefügt

---
+++ Produkt/JS/scriptLogin.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+
+	//Funktion für Login Button
+    $("#login_btn").click(function(){
+		var rolle;
+		if ($("#betreuer").prop("checked")) {
+			rolle = "betreuer";
+		}
+		else {
+			rolle = "proband";
+		}
+        var mailuid = $("#mailuid").val().trim();
+        var passwort = $("#passwort").val().trim();
+
+        if( mailuid != "" && passwort != "" ){
+            $.ajax({
+                url:'checkUser.php',
+                type:'post',
+                data:{mailuid:mailuid,passwort:passwort,rolle:rolle},
+                success:function(response){
+                    var msg = "";
+                    if(response == 1){
+						if (rolle == 'betreuer'){
+							window.location = "../php/index_betreuer.php"; //Link zur Startseite muss hier rein!
+						}
+						else if (rolle == 'proband'){
+							window.location = "../php/index_proband.php"; //Link zur Startseite muss hier rein!
+						}
+                    }
+					else{
+                        msg = "Die Kombination aus Username/Email und Passwort ist ungültig!";
+						$("#passwort").val("");
+                    }
+                    $("#message").html(msg);
+                }
+            });
+        }
+		else{
+            var msg = "Fehlende Eingaben!";
+			$("#message").html(msg);
+			$("#passwort").val("");
+		}
+    });
+});
