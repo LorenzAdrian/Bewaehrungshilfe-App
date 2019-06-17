5975277218a3cf93bb9562c575d1b6403d959389
DerAdek <50909696+DerAdek@users.noreply.github.com>
2019-06-17 16:46:44 +0200

    Update login.php

--- Produkt/PHP/login.php
+++ Produkt/PHP/login.php
@@ -1,40 +1,50 @@
 <!DOCTYPE html>
 <html>
 	<head>
+        <link rel="stylesheet" href="loginstyle.css">
+        
+            <link href="https://fonts.googleapis.com/css?family=Raleway&display=swap" rel="stylesheet">
 	</head>
+    <title>Login</title>
 
 	<body>
 		<main>
-			<div>
-				<h1>Login</h1>
+            <div class="logo"></div>
+			<div class="login">
+				<h1>LOGIN</h1>
 			</div>
 			<div id="message"></div>
-			<br>	
+			<br>
+            <div class="loginfenster">
 			<span>Sind Sie ein Betreuer?</span>
 			<input type="checkbox" name="rolle" id="betreuer" value="betreuer">
 			<br> 
 			<br>
 			<div id="error"></div>
-			<input type="text" name="mailuid" id="mailuid" placeholder="Username/Email">				
+			<input type="text" name="mailuid" id="mailuid" placeholder="Username/Email">
+            
+            <br>
 			<input type="password" name="passwort" id="passwort" placeholder="Passwort">
 			<br>
 			<br>
-			<input type="button"  name="login" id="login_btn" value="Login">		
-			
+			<input type="button"  name="login" id="login_btn" value="Login">
+            </div>
+            <div class="bild"></div>
+			<!--
 			<br>
 			<br>			
 			<div>
 				Neu hier?
-				<a href="signupcheck.php">Registrierung (nur zur Befüllung der Datenbank, wegen gehashtem Passwort)</a> 
-				<!-- <a href="signupRolle.php"> für JQuery Registrierung  -->
+				<a href="signupRolle.php">Registrierung</a>
 			</div>
-			
+			-->
 		</main>
 	</body>
 </html>
 
-<script src="../JS/jquery-3.4.1.js"></script>
-<script src="../JS/scriptLogin.js"></script>
+<script src="js/jquery-3.4.1.js"></script>
+<script src="js/scriptLogin.js"></script>
 
-
-
+<?php
+//require 'footer.php';
+?>
