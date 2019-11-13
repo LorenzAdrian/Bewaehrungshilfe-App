4956cdd7f29855726366f2a624f7486a9e6016ab
DerAdek <50909696+DerAdek@users.noreply.github.com>
2019-11-13 14:51:53 +0100

    Bootstrap

--- Produkt_Entwicklung/Entwicklung_Testversion/php/login.php
+++ Produkt_Entwicklung/Entwicklung_Testversion/php/login.php
@@ -8,12 +8,13 @@
 
 <!DOCTYPE html>
 <html>
-	<head>
 
+	<head>
+		
 	<!-- Required meta tags -->
 	<meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	
     <!-- Bootstrap CSS -->
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" 
 	integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
@@ -76,12 +77,12 @@
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 	
 	</body>
-	
+
 </html>
 
 
-<script src="../javascript/jquery-3.4.1.js"></script>
-<script src="../javascript/scriptLogin.js"></script>
+	<script src="../javascript/jquery-3.4.1.js"></script>
+	<script src="../javascript/scriptLogin.js"></script>
 
 <?php
 //require 'footer.php';
