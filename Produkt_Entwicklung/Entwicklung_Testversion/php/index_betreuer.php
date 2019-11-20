ee78213a0fb7306ae04dddec7454678f83a6edd7
DerAdek <50909696+DerAdek@users.noreply.github.com>
2019-11-20 12:52:48 +0100

    Header im probinfo.php und index_betreuer.php geändert

--- Produkt_Entwicklung/Entwicklung_Testversion/php/index_betreuer.php
+++ Produkt_Entwicklung/Entwicklung_Testversion/php/index_betreuer.php
@@ -27,7 +27,7 @@ if(isset($_POST['but_logout'])){
 
 <head>
 
-    <meta charset="utf-8">
+  <meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	
 	 <!-- Bootstrap CSS -->
@@ -38,19 +38,56 @@ if(isset($_POST['but_logout'])){
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     
+  <!--- Pfad zur style.css--------------------------->
+  <link rel="stylesheet" href="../CSS/Betreuer_index.css">
+  <!--Schriftart aus google fonts------------------>
+  <link href="https://fonts.googleapis.com/css?family=Raleway&display=swap" rel="stylesheet">
 
-
-    <!--- Pfad zur style.css--------------------------->
-    <link rel="stylesheet" href="../CSS/Betreuer_index.css">
-    <!--Schriftart aus google fonts------------------>
-    <link href="https://fonts.googleapis.com/css?family=Raleway&display=swap" rel="stylesheet">
-
-    <title>Start</title>
-    
-    <?php include '../includes/headerBet.php';?> 
+  <title>Start</title>
+     
 </head>
 
 <body>
+<!-- HEADER -->
+<header>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light flex-nowrap">
+
+    <a class="navbar-brand w-100" href="#">
+      <img src="../CSS/image/Baericon.jpeg"  width="30" height="30" alt="Logo" > &nbsp;&nbsp;Sodi 4u
+	  </a>
+
+     <div class="navbar-collapse collapse w-100" id="navbar5">
+        <ul class="navbar-nav mx-auto">
+		<!-- Aktuelle Seite -->
+      <li class="nav-item active">
+        <a class="nav-link" href="../php/index_betreuer.php">Startseite<span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="../php/probinfo.php">Termine</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="../php/probinfo.php">Nachrichten</a>
+      </li>
+	    <li class="nav-item">
+        <a class="nav-link" href="To_Do_Liste.php">Leitfaden</a>
+      </li>
+	    <li class="nav-item">
+        <a class="nav-link" href="login.php">Abmelden</a>
+      </li>
+    </ul>
+    </div>
+    <div class="w-100"><!--spacer--></div>
+
+
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar5" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  </nav>
+
+
+<hr>
+</header>
 
 <div class="logo"></div>
 <!---
