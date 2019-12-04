2c969fda68d9444bfdcbef0413a8fcd40d3e8f98
leizchristian <51290084+leizchristian@users.noreply.github.com>
2019-12-04 10:58:53 +0100

    BL247 Korrektur

--- Produkt_Entwicklung/Entwicklung_Testversion/php/meineNachrichtenAnzeigen.php
+++ Produkt_Entwicklung/Entwicklung_Testversion/php/meineNachrichtenAnzeigen.php
@@ -1,5 +1,49 @@
 <?php
-while ($dsatz = mysqli_fetch_assoc($result))
+require '../database/dbh.inc.php';
+
+if(!isset($_SESSION))
+{
+      session_start();
+}
+
+// Check user login or not
+if(!isset($_SESSION['userId'])){
+    header('Location: login.php');
+}
+
+// logout
+if(isset($_POST['but_logout'])){
+    session_destroy();
+    header('Location: Login.php');
+}
+
+if($_SESSION['rolle'] == 'proband') {
+	$personenID = 'PID';
+  $sender = 'betreuer';
+  $bsender = 1;
+}
+else {
+	$personenID = 'BID';
+  $sender ='proband';
+  $bsender = 0;
+}
+
+$event_sql =
+"SELECT N.NID, N.Zeitstempel, N.Text, N.Status, N.BSender, N.dateiname, S.Vorname AS vorname_sender, S.Nachname AS nachname_sender FROM nachricht AS N INNER JOIN ".$sender." AS S ON N.BID = S.BID WHERE N.".$personenID." = ".$_SESSION['userId']." ORDER BY Zeitstempel";
+
+//echo $event_sql;
+
+//Mit mysqli_query wird die SQL-Abfrage ausgeführt. Die Methode liefert ein Objekt der Klasse mysqli_result zurück. $result enthält die Referenz auf dieses Objekt.
+$res = mysqli_query($conn, $event_sql);
+
+//Jetzt wird der Status der Nachrichten auf 'gelesen' gesetzt
+$sql = "UPDATE nachricht SET Status='gelesen'
+WHERE ".$personenID." = ".$_SESSION['userId']." AND BSender=".$bsender;
+if ($conn->query($sql) != TRUE) {
+    echo "Es ist ein Fehler aufgetreten: ".$conn->error;
+}
+
+while ($dsatz = mysqli_fetch_assoc($res))
 {
   //Erzeugt aus der Zeichenkette des Zeitstempels ein DateTime-Objekt
   setlocale(LC_ALL, "");
@@ -7,21 +51,21 @@ while ($dsatz = mysqli_fetch_assoc($result))
 
   //Wenn die Nachricht ein String ist
   if(!$dsatz['dateiname']){
-  //Wenn der Betreuer der Sender der Nachricht und die Nachricht neu ist.
-    if($dsatz['BSender'] == 1 && $dsatz['Status'] == 'neu')
+  //Wenn der eingeloggte Benutzer der Empfänger der Nachricht und die Nachricht neu ist.
+    if($dsatz['BSender'] == $bsender && $dsatz['Status'] == 'neu')
       {
         echo '<table class="sender nachricht neu"><tr class="sendername"><td>'.$dsatz['vorname_sender'].' '.$dsatz['nachname_sender'].'</td></tr><tr class="nachricht"><td>'
             .$dsatz['Text'].'</td></tr><tr class="datumuhrzeit sender"><td>'
             .strftime('%a %e. %b %g, %H:%M', $datum).'</td></tr></table>';
         }
-      //Wenn der Betreuer der Sender der schon gelesenen Nachricht ist.
-      elseif($dsatz['BSender'] == 1)
+      //Wenn der eingeloggte Benutzer der Empfänger der schon gelesenen Nachricht ist.
+      elseif($dsatz['BSender'] == $bsender)
         {
           echo '<table class="sender nachricht"><tr class="sendername"><td>'.$dsatz['vorname_sender'].' '.$dsatz['nachname_sender'].'</td></tr><tr class="nachricht"><td>'
               .$dsatz['Text'].'</td></tr><tr class="datumuhrzeit sender"><td>'
               .strftime('%a %e. %b %g, %H:%M', $datum).'</td></tr></table>';
         }
-      //Wenn die Nachricht vom Probanden stammt.
+      //Wenn der eingeloggte Benutzer der Sender der Nachricht ist.
       else
         {
           echo '<table class="empfaenger nachricht"><tr class="nachricht"><td>'.$dsatz['Text'].'</td></tr><tr class="datumuhrzeit empfaenger"><td>'
@@ -30,34 +74,33 @@ while ($dsatz = mysqli_fetch_assoc($result))
     }
     //Wenn die Nachricht eine Datei ist
     else{
-    //Wenn der Betreuer der Sender der Nachricht und die Nachricht neu ist.
-    //$filename = $dsatz['dateiname'];
-    //file_put_contents($filename,$dsatz['image']);
-      if($dsatz['BSender'] == 1 && $dsatz['Status'] == 'neu')
+    //Wenn der eingeloggte Benutzer der Empfänger der Nachricht und die Nachricht neu ist.
+      if($dsatz['BSender'] == $bsender && $dsatz['Status'] == 'neu')
         {
           echo '<table class="sender nachricht neu">
           <tr class="sendername"><td>'.$dsatz['vorname_sender'].' '.$dsatz['nachname_sender'].'</td></tr>
-          <tr class="nachricht"><td><a href="datei_download.php?nid='.$dsatz['NID'].'" download="'.$dsatz['dateiname'].'">'.$dsatz['dateiname'].'</a></td></tr>
+          <tr class="nachricht"><td><a class="download" href="datei_download.php?nid='.$dsatz['NID'].'" download="'.$dsatz['dateiname'].'">'.$dsatz['dateiname'].'</a></td></tr>
           <tr class="datumuhrzeit sender"><td>'.strftime('%a %e. %b %g, %H:%M', $datum).'</td></tr></table>';
           }
-        //Wenn der Betreuer der Sender der schon gelesenen Nachricht ist.
-        elseif($dsatz['BSender'] == 1)
+        //Wenn der eingeloggte Benutzer der Empfänger der schon gelesenen Nachricht ist.
+        elseif($dsatz['BSender'] == $bsender)
           {
             echo '<table class="sender nachricht">
             <tr class="sendername"><td>'.$dsatz['vorname_sender'].' '.$dsatz['nachname_sender'].'</td></tr>
-            <tr class="nachricht"><td><a href="datei_download.php?nid='.$dsatz['NID'].'" download="'.$dsatz['dateiname'].'">'.$dsatz['dateiname'].'</a></td></tr>
+            <tr class="nachricht"><td><a class="download" href="datei_download.php?nid='.$dsatz['NID'].'" download="'.$dsatz['dateiname'].'">'.$dsatz['dateiname'].'</a></td></tr>
             <tr class="datumuhrzeit sender"><td>'.strftime('%a %e. %b %g, %H:%M', $datum).'</td></tr>
             </table>';
           }
-        //Wenn die Nachricht vom Probanden stammt.
+        //Wenn der eingeloggte Benutzer der Sender der Nachricht ist.
         else
           {
             echo '<table class="empfaenger nachricht">
-            <tr class="nachricht"><td><a href="datei_download.php?nid='.$dsatz['NID'].'" download="'.$dsatz['dateiname'].'">'.$dsatz['dateiname'].'</a></td></tr>
+            <tr class="nachricht"><td><a class="download" href="datei_download.php?nid='.$dsatz['NID'].'" download="'.$dsatz['dateiname'].'">'.$dsatz['dateiname'].'</a></td></tr>
             <tr class="datumuhrzeit empfaenger"><td>'.strftime('%a %e. %b %g, %H:%M', $datum).'</td></tr>
             </table>';
 
           }
       }
 }
+mysqli_close($conn);
 ?>
