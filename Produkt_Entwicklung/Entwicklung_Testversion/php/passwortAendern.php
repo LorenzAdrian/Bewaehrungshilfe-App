b750f5a81a979e7ce0d9239979462a115f4751e7
VanessaMllr <51118849+VanessaMllr@users.noreply.github.com>
2019-12-04 11:09:21 +0100

    BL 252: Seite Passwort ändern bearbeitet

--- Produkt_Entwicklung/Entwicklung_Testversion/php/passwortAendern.php
+++ Produkt_Entwicklung/Entwicklung_Testversion/php/passwortAendern.php
@@ -9,11 +9,17 @@ include 'dbh.inc.php';
 
 if(!isset($_SESSION))
 {
-      session_start();
+    session_start();
 }
 
 // Check user login or not
 if(!isset($_SESSION['userId'])){
+    header('Location: login.php');
+}
+
+// logout
+if(isset($_POST['but_logout'])){
+    session_destroy();
     header('Location: login.php');
 }
 ?>
@@ -25,6 +31,15 @@ if(!isset($_SESSION['userId'])){
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
+	<!--- Pfad zu den CSS Dateien--------------------------->
+    <link rel="stylesheet" href="../CSS/termine_proband.css">
+		<link rel="stylesheet" href="../CSS/header_prob.css">
+		<link rel="stylesheet" href="../CSS/footer_pro.css">
+		<link rel="stylesheet" href="../CSS/passwortAendern.css">
+    <!--Schriftart aus google fonts------------------------->
+    <link href="https://fonts.googleapis.com/css?family=Raleway&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet"> 
+	
 	 <!-- Bootstrap CSS -->
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
 	integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
@@ -33,26 +48,24 @@ if(!isset($_SESSION['userId'])){
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
-  <!-- Funktion, um Passwort anzuzeigen -->
-  <script src="../javascript/passwortAendern.js"></script>
-
-
+	<!-- Funktion, um Passwort anzuzeigen -->
+	<script src="../javascript/passwortAendern.js"></script>
 
 	<!--- Pfad zur style.css--------------------------->
-  <link rel="stylesheet" href="../CSS/index_proband.css">
+	<link rel="stylesheet" href="../CSS/index_proband.css">
 	<link rel="stylesheet" href="../CSS/header_prob.css">
-  <!--Schriftart aus google fonts------------------>
-  <link href="https://fonts.googleapis.com/css?family=Assistant:200,300,400,600,700&display=swap"  rel="stylesheet">
-  <!-- Stylesheet für Icons-->
+	<!--Schriftart aus google fonts------------------>
+	<link href="https://fonts.googleapis.com/css?family=Assistant:200,300,400,600,700&display=swap"  rel="stylesheet">
+	<!-- Stylesheet für Icons-->
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
-  <title>Passwort ändern</title>
+	<title>Passwort ändern</title>
 
-  <!-- Meldungen rot bzw. grün markieren -->
-  <style>
-  .error {color: #FF0000;}
-  .success {color: #00CC00;}
-  </style>
+	<!-- Meldungen rot bzw. grün markieren -->
+	<style>
+	.error {color: #FF0000;}
+	.success {color: #00CC00;}
+	</style>
 
 </head>
 <header>
@@ -84,18 +97,19 @@ if(!isset($_SESSION['userId'])){
 	    <li class="nav-item">
         <a class="nav-link" href="Notfall.php">Notfall</a>
       </li>
-<!-- Aktuelle Seite -->
-	   <li class="nav-item active">
+	<!-- Aktuelle Seite -->
+	  <li class="nav-item active">
         <a class="nav-link" href="passwortAendern.php">Passwort&nbsp;ändern<span class="sr-only">(current)</span></a>
       </li>
     </ul>
-
-    <form class="form-inline my-2 my-lg-0">
-      <button class="button-sodi btn-outline-sodi my-1 my-sm-0" type="submit">Abmelden</button>
-		</form>
+    <form class="form-inline my-2 my-lg-0" method='post' action="">
+        <button class="button-sodi btn-outline-sodi my-1 my-sm-0" name="but_logout" type="submit">Abmelden</button>
+	</form> 
   </div>
+  
   <div class="w-100">
-	</div>
+  </div>
+  
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar5" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -236,7 +250,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
       <tr>
         <td></td>
         <td>
-          <button type="submit" name="passwortAendern" id="passwortAendern" >Passwort ändern</button>
+          <button type="submit" name="passwortAendern" id="passwortAendern">Passwort ändern</button>
         </td>
       </tr>
     </table>
@@ -244,4 +258,25 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <div class="error"><?php echo $pwdTatErr;?></div>
   </form>
 </main>
+
+
+	<!--Footer-->
+	<footer id="sticky-footer" class="mb-0 mt-footer py-4 bg-light text-white-50">
+      <div class="pt-2 container text-center">
+
+              <ul>
+                <li>
+                  <a href="Impressum_Proband.php">Impressum</a>
+                </li>
+                <li>
+                    <a href="Datenschutz_Proband.php">Datenschutz</a>
+                </li>
+                <li>
+                  <a href="Kontakt_Proband.php">Kontakt</a>
+                </li>
+              </ul>
+
+      </div>
+    </footer>
+	
 </html>
