3b248be5b0da90c1f90653fcb2153a89ff0f7683
DerAdek <50909696+DerAdek@users.noreply.github.com>
2020-01-10 23:27:34 +0100

    Hintergrundfarbe des Headers geänder, damit das Logo ohne Rand angezeigt wird

--- Produkt_Entwicklung/Entwicklung_Testversion/php/index_betreuer.php
+++ Produkt_Entwicklung/Entwicklung_Testversion/php/index_betreuer.php
@@ -51,7 +51,7 @@ if(isset($_POST['but_logout'])){
 <!-- HEADER -->
 <header>
 
-<nav class="navbar navbar-expand-lg navbar-light bg-light flex-nowrap">
+<nav class="navbar navbar-expand-lg navbar-light flex-nowrap">
 
     <a class="navbar-brand w-100" href="#">
       <img src="../CSS/image/logoohneschatten.png"  width="50" height="50" alt="Logo" > &nbsp;&nbsp;SoDi4U
