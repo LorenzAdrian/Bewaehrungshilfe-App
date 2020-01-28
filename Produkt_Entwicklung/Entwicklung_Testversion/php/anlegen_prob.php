0d94958d88aaa64d966f3c38fdf485d182317225
richardsderek <richards.derek@gmail.com>
2020-01-28 16:14:41 +0100

    Code für Fehlermeldung "Username schon vergeben!" korrigiert. Weiterleitung mit Header (wieder?) auskommentiert

--- Produkt_Entwicklung/Entwicklung_Testversion/php/anlegen_prob.php
+++ Produkt_Entwicklung/Entwicklung_Testversion/php/anlegen_prob.php
@@ -34,7 +34,7 @@ if ($check !== "betreuer") {
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <!--Pfad zu ICON Fontawesome-->
-    <link rel="stylesheet" href="../CSS/css/all.min.css">  
+    <link rel="stylesheet" href="../CSS/css/all.min.css">
   <title>Proband anlegen</title>
 
 </head>
@@ -162,7 +162,7 @@ if (isset ($_POST['prob-self-submit'])){
 
       //Check, ob Username schon vergeben.
       if ($resultCheck > 0) {
-        header("Location: anlegen_bet.php?error=usertaken&mail=".$email);
+        //header("Location: anlegen_bet.php?error=usertaken&mail=".$email);
         //exit();
         $usernamevergeben = "Username schon vergeben!";
       }
